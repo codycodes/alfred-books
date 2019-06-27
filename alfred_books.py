e174b9b3db676ab0bd20d44df18608c413e0162a
codycodes <codycodes@users.noreply.github.com>
2019-06-27 00:57:46 -0700

    actually fix recognizing switches

--- alfred_books.py
+++ alfred_books.py
@@ -16,9 +16,12 @@ def main(wf):
 
     option = None
     if args and wf.args[0]:
-        switch = wf.args[0]
-        if ('-a' or '-t' or '-g' or '-h') in switch:
+        switch = wf.args[0].split()[0]
+        # log.debug('SWITCH: ' + switch)
+        switches = [u'-a',u'-t',u'-g',u'-h']
+        if any([switch in switches]):
             switch = switch[:2]
+            log.debug('SWITCH: ' + switch)
             query, option = wf.args[0].split(switch)[1], switch
         else:
             query, option = wf.args[0], None
@@ -27,19 +30,24 @@ def main(wf):
     # max age of 20 seconds to reduce querying database
     # and make it blazingly fast
     # books = wf.cached_data('books', book.get_books, max_age=20)
-    books = wf.cached_data('books', book.get_books)
+    # books = wf.cached_data('books', book.get_books) # Testing only...
+    books = book.get_books()
+    
 
-    # log.debug('OPTION: ' + str(option))
+    log.debug('QUERY: ' + str(query) + ', OPTION: ' + str(option))
     # TODO: play around with the text matching.
 
     # show help with no space required
     if query or option == '-h':
         if option:
             if option == '-a':
+                log.debug('-a input')
                 books = wf.filter(query, books, key=lambda book: u' '.join(book.author), min_score=30)
             elif option == '-t':
+                log.debug('-t input')
                 books = wf.filter(query, books, key=lambda book: u' '.join(book.title), min_score=30)
             elif option == '-g':
+                log.debug('-g input')
                 books = wf.filter(query, books, key=lambda book: u' '.join(book.genre), min_score=30)
             elif option == '-h':
                 wf.add_item(
